4a642b0afac25963dde8e4470cf03cb0222c887a
thuy <nguyenthithuyqptb@.com>
2020-02-12 16:13:49 +0700

    update 12/02/2020

--- matric-invoke.py
+++ matric-invoke.py
@@ -5,27 +5,15 @@ import numpy as np
 def matric_invoke(inFile, outFile):
     f = open(inFile, "r")
     count = sum(1 for line in open('api.txt'))
-    countdemo = 0
     arr = []
     for line in f:
         arr.append(line)
-    # print(arr)
     A = np.eye(count)
     for x in range(len(arr)):
         for y in range(len(arr)):
             if arr[x][0:12] == arr[y][0:12]:
                 A[x][y] = A[y][x] =1
-    # print(A)
     outFile.write(str(A))
-    #     i+=1
-    #     for line1 in f:
-    #         j+=1
-    #         if line[0:15] == line1[0:15]:
-    #             countdemo += 1
-    #             print(line + line1)
-    #         print(i,j)
-    # print(countdemo)
-    # outFile.write(str(A))
 
 pass
 inFile = "api.txt"
